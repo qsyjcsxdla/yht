bccd02e25da574b4760d2cf4b6f083e0282ea263
fcy <fcy_66@163.com>
2018-07-16 10:37:11 +0800

    新上传

---
+++ fcy_work/xyc/xinshoufuli/hello.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by D N on 2018/4/26.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var hello = /** @class */ (function () {
+    function hello() {
+    }
+    return hello;
+}());
+exports.hello = hello;
---
+++ fcy_work/xyc/xinshoufuli/hello.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by D N on 2018/4/26.
+ */
+
+
+export class hello{
+
+}
